144db8a9acf78faaeaf2831906e92d982fb1db5f
Paul Bunting <paulbunting@pauls-imac.bigpond>
2014-09-06 22:42:43 +1000

    Initial Commit

---
+++ PTNotebook2/OHTAppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  OHTAppDelegate.h
+//  PTNotebook2
+//
+//  Created by Paul Bunting on 6/09/2014.
+//  Copyright (c) 2014 100 Trees. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface OHTAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+@end
---
+++ PTNotebook2/OHTMasterViewController.h
@@ -0,0 +1,22 @@
+//
+//  OHTMasterViewController.h
+//  PTNotebook2
+//
+//  Created by Paul Bunting on 6/09/2014.
+//  Copyright (c) 2014 100 Trees. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class OHTDetailViewController;
+
+#import <CoreData/CoreData.h>
+
+@interface OHTMasterViewController : UITableViewController <NSFetchedResultsControllerDelegate>
+
+@property (strong, nonatomic) OHTDetailViewController *detailViewController;
+
+@property (strong, nonatomic) NSFetchedResultsController *fetchedResultsController;
+@property (strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+
+@end
